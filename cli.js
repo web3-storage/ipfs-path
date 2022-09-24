07cd18d15b162ddaf5a53ae8b5d186ebeafb64fd
Alan Shaw <alan.shaw@protocol.ai>
2022-09-23 19:45:59 -0700

    feat: streaming export

--- cli.js
+++ cli.js
@@ -5,14 +5,13 @@ import colors from 'colors'
 import { extract } from './index.js'
 import * as raw from 'multiformats/codecs/raw'
 import * as dagPb from '@ipld/dag-pb'
-import { CarIndexedReader } from '@ipld/car/indexed-reader'
-import { CarWriter } from '@ipld/car/writer'
+import { CarBlockIterator, CarWriter, CarIndexedReader } from '@ipld/car'
 import { CID } from 'multiformats/cid'
 import { decode as blockDecode } from 'multiformats/block'
 import { sha256 as hasher } from 'multiformats/hashes/sha2'
-
-import { createWriteStream } from 'fs'
+import { createReadStream, createWriteStream } from 'fs'
 import { Readable } from 'stream'
+import { exporter } from 'ipfs-unixfs-exporter'
 
 const Decoders = {
   [raw.code]: raw,
@@ -22,19 +21,16 @@ const Decoders = {
 const cli = sade('ipfs-car-tools')
 
 cli.command('extract <path> <car>')
-  .describe('Extract ipfs path blocks from a CAR')
-  .example('ipfs-car-extract bafybeig5uisjbc25pkjwtyq5goocmwr7lz5ln63llrtw4d5s2y7m7nhyeu/path/to/image.png my.car > image.png.car')
+  .describe('Extract IPFS path blocks from a CAR')
+  .example('ipfs-car-tools extract bafybeig5uisjbc25pkjwtyq5goocmwr7lz5ln63llrtw4d5s2y7m7nhyeu/path/to/image.png my.car > image.png.car')
   .option('-o, --output', 'Output path for CAR')
   .action(async (path, car, options) => {
     const reader = await CarIndexedReader.fromFile(car)
     const blocks = extract(reader, path)
     const { writer, out } = CarWriter.create(getRootCidFromPath(path))
+    const dest = options.output ? createWriteStream(options.output) : process.stdout
 
-    if (options.output) {
-      Readable.from(out).pipe(createWriteStream(options.output))
-    } else {
-      Readable.from(out).pipe(process.stdout)
-    }
+    Readable.from(out).pipe(dest)
 
     for await (const block of blocks) {
       await writer.put(block)
@@ -79,6 +75,28 @@ cli.command('tree <car>')
     }
 
     console.log(archy(archyRoot))
+  })
+
+cli.command('export <path> <car>')
+  .describe('Export a UnixFS file from a CAR')
+  .example('ipfs-car-tools export bafybeig5uisjbc25pkjwtyq5goocmwr7lz5ln63llrtw4d5s2y7m7nhyeu/path/to/image.png image.png.car > image.png')
+  .option('-o, --output', 'Output path for file')
+  .action(async (path, car, options) => {
+    const blocks = (await CarBlockIterator.fromIterable(createReadStream(car)))[Symbol.asyncIterator]()
+    const blockstore = {
+      async get (key) {
+        const { done, value } = await blocks.next()
+        if (done) throw new Error('unexpected EOF')
+        if (value.cid.toString() !== key.toString()) {
+          throw new Error(`CID mismatch, expected: ${key}, received: ${value.cid}`)
+        }
+        return value.bytes
+      }
+    }
+    const entry = await exporter(path, blockstore)
+    if (entry.type === 'directory') throw new Error(`${path} is a directory`)
+    const dest = options.output ? createWriteStream(options.output) : process.stdout
+    Readable.from(entry.content()).pipe(dest)
   })
 
 cli.parse(process.argv)
